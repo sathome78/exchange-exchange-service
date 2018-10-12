907e35b58e03e39ec05504b4eb68ead742a3b73f
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2018-10-12 12:41:54 +0300

    create new Currency Exchange project

---
+++ src/main/java/me/exrates/exchange/ExchangeApplication.java
@@ -0,0 +1,12 @@
+package me.exrates.exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeApplication.class, args);
+    }
+}
