9add81ae6fcad7a98d1055e271b218ace56722ae
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-05-01 18:30:17 +0300

    min fix

--- src/main/java/me/exrates/exchange/controllers/MainController.java
+++ src/main/java/me/exrates/exchange/controllers/MainController.java
@@ -1,9 +1,9 @@
 package me.exrates.exchange.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 
     @RequestMapping("/")
