0c634fe9646bfd446ac5ab753c80bdf93c813411
olegkostiukevych <oleg.kostiukevych@upholding.biz>
2019-05-01 18:58:51 +0300

    min fix

--- src/main/java/me/exrates/exchange/controllers/CurrencyController.java
+++ src/main/java/me/exrates/exchange/controllers/CurrencyController.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -34,6 +35,7 @@ import static java.util.stream.Collectors.toMap;
 import static org.springframework.http.MediaType.APPLICATION_OCTET_STREAM_VALUE;
 
 @RestController
+@CrossOrigin(value = {"http://localhost:8080", "http://172.50.10.10:8080"})
 @RequestMapping("/currency")
 public class CurrencyController {
 
--- src/main/java/me/exrates/exchange/controllers/MainController.java
+++ src/main/java/me/exrates/exchange/controllers/MainController.java
@@ -1,12 +1,15 @@
 package me.exrates.exchange.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@CrossOrigin(value = {"http://localhost:8080", "http://172.50.10.10:8080"})
 public class MainController {
 
-    @RequestMapping("/")
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
         return "index";
     }
